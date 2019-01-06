5b35c73d2579a1f0650f98876a4976156777b51b
Alberto Lusiani <alusiani@gmail.com>
2019-01-06 15:06:47 +0100

    Nikola auto commit.
    Nikola version: 7.8.15

--- conf.py
+++ conf.py
@@ -130,7 +130,6 @@ TRANSLATIONS_PATTERN = "{path}.{lang}.{ext}"
 #          else they won’t be highlighted when active.
 NAVIGATION_LINKS = {
   DEFAULT_LANG: (
-    # ("link://post_path/posts", "Posts"), # +++ does not work
     ("/posts/", "Posts"),
     ("/tags/", "Tags"),
     ("/archive/", "Archive"),
@@ -145,9 +144,15 @@ NAVIGATION_LINKS = {
 }
 
 #
-# Not using path handlers above since with them highlighting of the link
+# could not find how to get a path handler for posts
+# ("link://post_path/posts", "Posts"),
+#
+
+#
+# not using path handlers above since with them highlighting of the link
 # does not work in Nikola v7.8.15.  It depends on the test
 # in nikola/data/themes/bootstrap3/templates/pagination_helper.tmpl:
+#
 # <li ${' class="active"' if i == current_page else ''}>
 #   <a href="${link}">${i + 1}${' <span class="sr-only">(current)</span>'
 #  if i == current_page else ''}</a></li>
